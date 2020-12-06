404904ed84fff07d98c5a6585a8bb8384dab58a8
Ray Messina <rmessina10@gmail.com>
2020-12-06 01:08:00 -0600

    Created started Main Component, ported CSS from boostrap version into App.css

---
+++ src/components/main.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+
+    render() {
+        return (
+            <p>Shell</p >
+        );
+    }
+}
+
+export default Main;
---
+++ src/shared/siteData.js
@@ -0,0 +1,93 @@
+const siteData = {
+
+    log: {
+        isLogged: true,
+        userData: {
+            name: 'a name string',
+            id: 12345,
+            key: 'passw'
+        }
+    },
+    info: {
+        copyright: 2020,
+        siteName: 'Kupboard',
+        author: 'Ray Messina Design',
+        authorURL: 'raymessinadesign.com'
+    },
+    social:
+        [{
+            id: 1,
+            name: "Facebook",
+            icon: "fa fa-facebook",
+            url: "facebook.com/somama"
+        },
+        {
+            id: 2,
+            name: "Instagram",
+            icon: "fa fa-instagram",
+            url: "instagram.com/somama"
+        },
+        {
+            id: 3,
+            name: "Twitter",
+            icon: "fa fa-twitter",
+            url: "twitter.com/somama"
+        },
+        {
+            id: 4,
+            name: "Google Plus",
+            icon: "fa fa-google-plus",
+            url: "googleplus.com/somama"
+        },
+        {
+            id: 5,
+            name: "Share",
+            icon: "fa fa-share-alt",
+            url: "facebook.com/somama"
+        }]
+    ,
+    pages: [{
+        id: 1,
+        text: "Home",
+        icon: "fa fa-house",
+        url: "index.html",
+        include: [],
+        exclude: []
+    },
+    {
+        id: 2,
+        text: "About",
+        icon: "fa fa-house",
+        url: "about.html",
+        include: [],
+        exclude: []
+    },
+    {
+        id: 3,
+        text: "Find",
+        icon: "fa fa-house",
+        url: "find.html",
+        include: [],
+        exclude: []
+    },
+    {
+        id: 4,
+        text: "Join",
+        icon: "fa fa-house",
+        url: "signup.html",
+        include: [],
+        exclude: []
+    },
+    {
+        id: 5,
+        text: "TOS",
+        icon: "fa fa-doc",
+        url: "tos.html",
+        include: ['footer'],
+        exclude: []
+    }],
+    nextID: {
+        pages: 6,
+        social: 6
+    }
+}
